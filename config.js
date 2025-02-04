fac7f3f908c9cac46f664b74057c51059575a0b6
Quang Duy <45655321+yugiking0@users.noreply.github.com>
2025-02-04 11:23:16 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: 'localhost',
+    user: 'root', // Thay bằng user MySQL của bạn
+    password: 'Frdn@2025', // Thay bằng password MySQL của bạn
+    database: 'inventory'
+};
